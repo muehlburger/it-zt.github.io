c7e21f9655efc14af2f9f2d019c872f21d681d5c
Herbert Mühlburger <mail@muehlburger.at>
2025-07-21 12:36:16 +0200

    Rebuilding site at Mo 21 Jul 2025 12:36:16 CEST

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
-var body = document.querySelector('body')
-var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
-var menuContainer = document.querySelector('#main-menu-mobile');
+var body = document.querySelector("body");
+var menuTrigger = document.querySelector("#toggle-main-menu-mobile");
+var menuContainer = document.querySelector("#main-menu-mobile");
 
-menuTrigger.onclick = function() {
-    menuContainer.classList.toggle('open');
-    menuTrigger.classList.toggle('is-active')
-    body.classList.toggle('lock-scroll')
+menuTrigger.onclick = function () {
+  menuContainer.classList.toggle("open");
+  menuTrigger.classList.toggle("is-active");
+  body.classList.toggle("lock-scroll");
+};
+
+var header = document.getElementById("stickyHeader");
+
+if (header) {
+  var offset = header.offsetTop;
+  document.addEventListener("scroll", function () {
+    if (window.scrollY > offset) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
 }
